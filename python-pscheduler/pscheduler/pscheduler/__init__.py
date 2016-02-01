a70de036da853ba05bdba5713d0d2c5b0df3f5a9
Mark Feit <mfeit@internet2.edu>
2016-02-01 07:03:16 -0500

    Use library routine for establishing connection to the database.

--- python-pscheduler/pscheduler/pscheduler/__init__.py
+++ python-pscheduler/pscheduler/pscheduler/__init__.py
@@ -1,8 +1,9 @@
+#
+# Initialization for pScheduler Python Package
+#
 
-
+from .db import *
 from .exitstatus import *
 from .iso8601 import *
 from .psjson import *
 from .timedelta import *
-
-
---
+++ python-pscheduler/pscheduler/pscheduler/db.py
@@ -0,0 +1,48 @@
+"""
+Functions for connecting to the pScheduler database
+"""
+
+import psycopg2
+
+def pg_connection(dsn='', autocommit=True):
+    """
+    Connect to the database, and return a handle to it
+
+    Arguments:
+
+    dsn - A data source name to use in connecting to the database.  If
+    the string begins with an '@', the remainder will be treated as
+    the path to a file where the value can be retrieved.
+
+    autocommit - Whether or not commits are done automatically when
+    quesies are issued.
+    """
+
+    # Read the DSN from a file if requested
+    if dsn.startswith('@'):
+        with open(dsn[1:], 'r') as dsnfile:
+            dsn = dsnfile.read().replace('\n', '')
+
+    pg = psycopg2.connect(dsn)
+    if autocommit:
+        pg.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
+
+    return pg
+
+
+def pg_cursor(dsn='', autocommit=True):
+    """
+    Connect to the database, and return a cursor.
+
+    Arguments:
+
+    dsn - A data source name to use in connecting to the database.  If
+    the string begins with an '@', the remainder will be treated as
+    the path to a file where the value can be retrieved.
+
+    autocommit - Whether or not commits are done automatically when
+    quesies are issued.
+    """
+
+    pg = pg_connection(dsn, autocommit)
+    return pg.cursor()
