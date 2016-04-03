9f323dae623e38261a1a63016b8447c96fe021b4
Mark Feit <mfeit@internet2.edu>
2016-04-03 19:47:47 -0400

    Return fully-qualified URLS.  Doc changes.

--- python-pscheduler/pscheduler/pscheduler/api.py
+++ python-pscheduler/pscheduler/pscheduler/api.py
@@ -2,19 +2,23 @@
 Functions related to the pScheduler REST API
 """
 
+import socket
+
 def api_root():
+    "Return the standard root location of the pScheduler hierarchy"
     return '/pscheduler'
 
 
-def api_url(host = None,  # Don't default this.  It breaks 'None' behavior.
+def api_url(host = None,
             path = None,
             port = None,
             protocol = 'http'
             ):
+    """Format a URL for use with the pScheduler API."""
     if path is not None and path.startswith('/'):
         path = path[1:]
     return protocol + '://' \
-        + ('127.0.0.1' if host is None else str(host)) \
+        + (socket.getfqdn() if host is None else str(host)) \
         + ('' if port is None else (':' + str(port))) \
         + api_root() + '/'\
         + ('' if path is None else str(path))
@@ -26,3 +30,5 @@ if __name__ == "__main__":
     print api_url(host='host.example.com', path='/both-slash')
     print api_url(host='host.example.com', path='both-noslash')
     print api_url(path='nohost')
+    print
+    print api_full_host()
