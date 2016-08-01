b9d26d127e3911e1d9e91cf7518dd09ebf8927a3
Mark Feit <mfeit@internet2.edu>
2016-08-01 11:22:43 -0400

    Re-establish the database connection if the cursor comes up closed.  This will prevent any server thread from coughing up more than one error if the connection is reset.  Can't make it any more reliable without doing a round-trip to the server each time.  Related to #58.

--- pscheduler-api-server/pscheduler-api-server/pschedulerapiserver/dbcursor.py
+++ pscheduler-api-server/pscheduler-api-server/pschedulerapiserver/dbcursor.py
@@ -5,15 +5,22 @@
 import pscheduler
 import sys
 
+from .log import log
+
 this = sys.modules[__name__]
 
+this.dsn = None
 this.cursor = None
 
 def dbcursor_init(dsn):
     """Connect to the database"""
     if this.cursor is None:
         this.cursor = pscheduler.pg_cursor(dsn)
-        # TODO: What happens if the connection goes south?
+        this.dsn = dsn
 
 def dbcursor():
+    if this.cursor.closed:
+        log.warning("Database cursor is closed; reconnecting")
+        this.cursor = None
+        dbcursor_init(this.dsn)
     return this.cursor
