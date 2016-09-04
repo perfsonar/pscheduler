cda33334f97b3030c52ac87d8b9d2d0f4d47a0fa
Mark Feit <mfeit@internet2.edu>
2016-09-04 09:43:05 -0400

    Disconnect clients with less fanfare

--- pscheduler-server/pscheduler-server/database/database-drop.sql
+++ pscheduler-server/pscheduler-server/database/database-drop.sql
@@ -4,11 +4,15 @@
 
 -- Force disconnection of any clients using the database.
 
-SELECT pg_terminate_backend(pg_stat_activity.pid)
-FROM pg_stat_activity
-WHERE
-    pg_stat_activity.datname = 'pscheduler'
-    AND pid <> pg_backend_pid();
+DO $$
+BEGIN
+    PERFORM pg_terminate_backend(pg_stat_activity.pid)
+    FROM pg_stat_activity
+    WHERE
+        pg_stat_activity.datname = 'pscheduler'
+        AND pid <> pg_backend_pid();
+END;
+$$ LANGUAGE plpgsql;
 
 
 DROP DATABASE IF EXISTS pscheduler;
