993638c22d482bd0e797137cbfbccea5f6f8d43e
Mark Feit <mfeit@internet2.edu>
2016-09-04 09:15:57 -0400

    Forcibly disconnect clients from the database before dropping it.

--- pscheduler-server/pscheduler-server/database/database-drop.sql
+++ pscheduler-server/pscheduler-server/database/database-drop.sql
@@ -2,5 +2,14 @@
 -- Database drop
 --
 
+-- Force disconnection of any clients using the database.
+
+SELECT pg_terminate_backend(pg_stat_activity.pid)
+FROM pg_stat_activity
+WHERE
+    pg_stat_activity.datname = 'pscheduler'
+    AND pid <> pg_backend_pid();
+
+
 DROP DATABASE IF EXISTS pscheduler;
 DROP ROLE IF EXISTS pscheduler;
