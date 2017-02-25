e79edce9517c0fd2152bd74c0600e2d460c7409c
Andy Lake <andrewrlake@gmail.com>
2017-02-25 13:34:36 -0500

    Fixing some syntax errors that break compile

--- pscheduler-test-dns/dns/test.py
+++ pscheduler-test-dns/dns/test.py
@@ -9,120 +9,120 @@ if __name__ == "__main__":
     sample = {
         "schema": 1,
         "succeeded": True,
-	"record": [ ]
+        "record": [ ]
         }
     print validate.result_is_valid( sample )
 
     sample = {
         "schema": 1,
         "succeeded": True,
-	"record": [ "" ]
+        "record": [ "" ]
         }
     print validate.result_is_valid( sample )
 
     sample = {
         "schema": 1,
         "succeeded": True,
-	"record": "hostname.com"
+        "record": "hostname.com"
         }
     print validate.result_is_valid( sample )
 
     sample = {
         "schema": 1,
         "succeeded": True,
-	"record": [ "hostname.com" ]
+        "record": [ "hostname.com" ]
         }
     print validate.result_is_valid( sample )
 
     sample = {
         "schema": 1,
         "succeeded": True,
-	"record": [ "hostname.com", "over.yonder.org" ]
+        "record": [ "hostname.com", "over.yonder.org" ]
         }
     print validate.result_is_valid( sample )
 
     sample = {
         "schema": 1,
         "succeeded": True,
-	"record": [ 1, "hostname.com", 2, "over.yonder.org" ]
+        "record": [ 1, "hostname.com", 2, "over.yonder.org" ]
         }
     print validate.result_is_valid( sample )
 
     sample = {
         "schema": 1,
         "succeeded": True,
-	"record": [ { 1, "hostname.com" } ]
+        "record": [ [ 1, "hostname.com" ] ]
         }
     print validate.result_is_valid( sample )
 
     sample = {
         "schema": 1,
         "succeeded": True,
-	"record": [ { 1, "hostname.com" } ]
+        "record": [ [ 1, "hostname.com" ] ]
         }
     print validate.result_is_valid( sample )
 
     sample = {
         "schema": 1,
         "succeeded": True,
-	"record": [ { 1, "hostname.com" } ]
+        "record": [ [ 1, "hostname.com" ] ]
         }
     print validate.result_is_valid( sample )
 
     sample = {
         "schema": 1,
         "succeeded": True,
-	"record": [ [ 1, "hostname.com" ] ]
+        "record": [ [ 1, "hostname.com" ] ]
         }
     print validate.result_is_valid( sample )
 
     sample = {
         "schema": 1,
         "succeeded": True,
-	"record": [ "domain.com", "owner", 1, 2, 3, 4, 5, 6 ]
+        "record": [ "domain.com", "owner", 1, 2, 3, 4, 5, 6 ]
         }
     print validate.result_is_valid( sample )
 
     sample = {
         "schema": 1,
         "succeeded": True,
-	"record": [ "domain.com", "owner", 1, 2, 3, 4, 5 ]
+        "record": [ "domain.com", "owner", 1, 2, 3, 4, 5 ]
         }
     print validate.result_is_valid( sample )
 
     sample = {
         "schema": 1,
         "succeeded": True,
-	"record": [ "domain.com", "owner", "1", "2", "3", "4", "5" ]
+        "record": [ "domain.com", "owner", "1", "2", "3", "4", "5" ]
         }
     print validate.result_is_valid( sample )
 
     sample = {
         "schema": 1,
         "succeeded": True,
-	"record": [ "txt" ]
+        "record": [ "txt" ]
         }
     print validate.result_is_valid( sample )
 
     sample = {
         "schema": 1,
         "succeeded": True,
-	"record": [ "txt", "more text" ]
+        "record": [ "txt", "more text" ]
         }
     print validate.result_is_valid( sample )
 
     sample = {
         "schema": 1,
         "succeeded": True,
-	"record": [ { "pref": 1, "mx" : "hostname.com" } ]
+        "record": [ { "pref": 1, "mx" : "hostname.com" } ]
         }
     print validate.result_is_valid( sample )
 
     sample = {
         "schema": 1,
         "succeeded": True,
-	"record": [ { "pref": 1, "mx" : "hostname.com" },
-		{ "pref": 2, "mx" : "somewhere.com" } ]
+        "record": [ { "pref": 1, "mx" : "hostname.com" },
+        { "pref": 2, "mx" : "somewhere.com" } ]
         }
     print validate.result_is_valid( sample )
 
