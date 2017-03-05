47d9269cb93e43070607fd9937fb4d9104b1044d
Mark Feit <mfeit@internet2.edu>
2017-03-05 17:19:36 -0500

    Fixed handling of pipe errors when writing to subprocess.  #255

--- python-pscheduler/pscheduler/pscheduler/program.py
+++ python-pscheduler/pscheduler/pscheduler/program.py
@@ -47,6 +47,106 @@ def __running_drop(process):
 
 
 
+# Only used in _Popen
+import errno
+import os
+
+class _Popen(subprocess32.Popen):
+    """
+    Improved version of subprocess32's Popen that handles SIGPIPE
+    without throwing an exception.
+    """
+
+    def _communicate_with_poll(self, input, endtime, orig_timeout):
+
+        # This is just to maintain the indentation of the original.
+        if True:
+
+            stdout = None # Return
+            stderr = None # Return
+
+            if not self._communication_started:
+                self._fd2file = {}
+
+            poller = select.poll()
+            def register_and_append(file_obj, eventmask):
+                poller.register(file_obj.fileno(), eventmask)
+                self._fd2file[file_obj.fileno()] = file_obj
+
+            def close_unregister_and_remove(fd):
+                poller.unregister(fd)
+                self._fd2file[fd].close()
+                self._fd2file.pop(fd)
+
+            if self.stdin and input:
+                register_and_append(self.stdin, select.POLLOUT)
+
+            # Only create this mapping if we haven't already.
+            if not self._communication_started:
+                self._fd2output = {}
+                if self.stdout:
+                    self._fd2output[self.stdout.fileno()] = []
+                if self.stderr:
+                    self._fd2output[self.stderr.fileno()] = []
+
+            select_POLLIN_POLLPRI = select.POLLIN | select.POLLPRI
+            if self.stdout:
+                register_and_append(self.stdout, select_POLLIN_POLLPRI)
+                stdout = self._fd2output[self.stdout.fileno()]
+            if self.stderr:
+                register_and_append(self.stderr, select_POLLIN_POLLPRI)
+                stderr = self._fd2output[self.stderr.fileno()]
+
+            # Save the input here so that if we time out while communicating,
+            # we can continue sending input if we retry.
+            if self.stdin and self._input is None:
+                self._input_offset = 0
+                self._input = input
+                if self.universal_newlines and isinstance(self._input, unicode):
+                    self._input = self._input.encode(
+                            self.stdin.encoding or sys.getdefaultencoding())
+
+            while self._fd2file:
+                try:
+                    ready = poller.poll(self._remaining_time(endtime))
+                except select.error, e:
+                    if e.args[0] == errno.EINTR:
+                        continue
+                    raise
+                self._check_timeout(endtime, orig_timeout)
+
+                for fd, mode in ready:
+                    if mode & select.POLLOUT:
+                        chunk = self._input[self._input_offset :
+                                            self._input_offset
+                                            + subprocess32._PIPE_BUF]
+
+                        # Handle EPIPE, because having this module
+                        # restore the signals doesn't seem to work.
+
+                        try:
+                            self._input_offset += os.write(fd, chunk)
+                            if self._input_offset >= len(self._input):
+                                close_unregister_and_remove(fd)
+                        except OSError as ex:
+                            if ex.errno != errno.EPIPE:
+                                raise ex
+                            close_unregister_and_remove(fd)
+
+                    elif mode & select_POLLIN_POLLPRI:
+                        data = os.read(fd, 4096)
+                        if not data:
+                            close_unregister_and_remove(fd)
+                        self._fd2output[fd].append(data)
+                    else:
+                        # Ignore hang up or errors.
+                        close_unregister_and_remove(fd)
+
+            return (stdout, stderr)
+
+
+
+
 def run_program(argv,              # Program name and args
                 stdin=None,        # What to send to stdin
                 line_call=None,    # Lambda to call when a line arrives
@@ -83,11 +183,11 @@ def run_program(argv,              # Program name and args
         raise Exception("Can't run with null arguments.")
 
     try:
-        process = subprocess32.Popen(argv,
-                                     stdin=subprocess32.PIPE,
-                                     stdout=subprocess32.PIPE,
-                                     stderr=subprocess32.PIPE,
-                                     )
+        process = _Popen(argv,
+                         stdin=subprocess32.PIPE,
+                         stdout=subprocess32.PIPE,
+                         stderr=subprocess32.PIPE
+                     )
 
         __running_add(process)
 
@@ -132,13 +232,17 @@ def run_program(argv,              # Program name and args
 
             fds = [ stdout_fileno, stderr_fileno ]
 
-            end_time = pscheduler.time_now() \
-                + pscheduler.seconds_as_timedelta(timeout)
+            if timeout is not None:
+                end_time = pscheduler.time_now() \
+                           + pscheduler.seconds_as_timedelta(timeout)
+            else:
+                time_left = None
 
             while True:
 
-                time_left = pscheduler.timedelta_as_seconds(
-                    end_time - pscheduler.time_now() )
+                if timeout is not None:
+                    time_left = pscheduler.timedelta_as_seconds(
+                        end_time - pscheduler.time_now() )
 
                 reads, writes, specials = select.select(fds, [], [], time_left)
 
@@ -158,6 +262,13 @@ def run_program(argv,              # Program name and args
 
                 if process.poll() != None:
                     break
+
+            # Siphon off anything left on stdout
+            while True:
+                line = process.stdout.readline()
+                if line == '':
+                    break
+                line_call(line[:-1])
 
             process.wait()
 
@@ -193,19 +304,64 @@ if __name__ == "__main__":
     for sig in [ signal.SIGHUP, signal.SIGINT, signal.SIGQUIT, signal.SIGTERM ]:
         signal.signal(sig, exit_handler)
 
-    status, out, err = run_program(["cat", "/etc/issue", "-"],
-                                   stdin="Hello, world.",
-                                   short=True
-                                   )
-    print "Status:", status
-    print "Out:   ", out
-    print "Err:   ", err
 
 
-    # Use this when testing termination
-    if False:
-        status, out, err = run_program(["sleep", "15"])
-        print "Status:", status
-        print "Out:   ", out
-        print "Err:   ", err
+    def dump_result(title, tup):
+        print
+        print "#\n# %s\n#" % (title)
+        (status, stdout, stderr) = tup
+        print "Exited", status
+        print "Stdout", stdout
+        print "Stderr", stderr
 
+    do_all = True
+
+    if do_all or False:
+        dump_result(
+            "Plain run",
+            run_program(
+                ['id']
+            ))
+
+
+    if do_all or False:
+        dump_result(
+            "Failure with stderr",
+            run_program(
+                ['ls', '/bad/path']
+            ))
+
+
+    if do_all or False:
+        dump_result(
+            "Timeout",
+            run_program(
+                ['sleep', '2'],
+                timeout=1
+            ))
+
+
+    if do_all or False:
+        lines = []
+        def line_writer(line):
+            lines.append(line)
+
+        dump_result(
+            "Line-at-a-time",
+            run_program(
+                ['ls', '/'],
+                line_call=line_writer
+            ))
+
+        print "Lines:"
+        print "\n".join(["  %s" % (line) for line in lines])
+
+
+    if do_all or False:
+        inp = "\n".join([str(number) for number in range(1, 1000000)])
+        dump_result(
+            "Induced pipe error (Should show no errors)",
+            run_program(
+                ['head', '-10'],
+                stdin=inp
+            ))
