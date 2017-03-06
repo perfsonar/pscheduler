e7382c84e02537cf4713886c7979951505aa38c0
Monte Goode <MMGoode@lbl.gov>
2017-03-06 11:06:50 -0800

    Jsonval module testing.

--- python-pscheduler/pscheduler/tests/jsonval_test.py
+++ python-pscheduler/pscheduler/tests/jsonval_test.py
@@ -6,15 +6,64 @@ import unittest
 
 from base_test import PschedTestBase
 
+from pscheduler.jsonval import json_validate
+
 
 class TestJsonval(PschedTestBase):
     """
     Jsonval tests.
     """
 
-    def test_one(self):
+    def test_jsonval(self):
         """"""
-        pass
+        sample = {
+            "schema": 1,
+            "when": "2015-06-12T13:48:19.234",
+            "howlong": "PT10M",
+            "sendto": "bob@example.com",
+            "x-factor": 3.14,
+            "protocol": "udp",
+            "ipv": 6,
+            "ip": "fc80:dead:beef::",
+            "archspec": {"name": "foo", "data": None},
+        }
+
+        schema = {
+            "local": {
+                "protocol": {
+                    "type": "string",
+                    "enum": ['icmp', 'udp', 'tcp']
+                }
+            },
+            "type": "object",
+            "properties": {
+                "schema": {"$ref": "#/pScheduler/Cardinal"},
+                "when": {"$ref": "#/pScheduler/Timestamp"},
+                "howlong": {"$ref": "#/pScheduler/Duration"},
+                "sendto": {"$ref": "#/pScheduler/Email"},
+                "ipv": {"$ref": "#/pScheduler/ip-version"},
+                "ip": {"$ref": "#/pScheduler/IPAddress"},
+                "protocol": {"$ref": "#/local/protocol"},
+                "x-factor": {"type": "number"},
+                "archspec": {"$ref": "#/pScheduler/ArchiveSpecification"},
+
+            },
+            "required": ["sendto", "x-factor"]
+        }
+
+        # missing an attr
+        valid, message = json_validate(sample, schema)
+
+        self.assertFalse(valid)
+        self.assertEqual(message, "At /archspec: 'archiver' is a required property")
+
+        # add it in now
+        sample['archspec']['archiver'] = 'tar'
+
+        valid, message = json_validate(sample, schema)
+
+        self.assertTrue(valid)
+        self.assertEqual(message, 'OK')
 
 
 if __name__ == '__main__':
