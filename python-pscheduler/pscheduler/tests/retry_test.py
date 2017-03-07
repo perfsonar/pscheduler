272fa3048cf04994109ebeca564cb8c50b1a8529
Monte Goode <MMGoode@lbl.gov>
2017-03-07 08:29:32 -0800

    Retry policy test.

--- python-pscheduler/pscheduler/tests/retry_test.py
+++ python-pscheduler/pscheduler/tests/retry_test.py
@@ -2,9 +2,12 @@
 test for the Retry module.
 """
 
+import datetime
 import unittest
 
 from base_test import PschedTestBase
+
+from pscheduler.retry import RetryPolicy
 
 
 class TestRetry(PschedTestBase):
@@ -12,9 +15,24 @@ class TestRetry(PschedTestBase):
     Retry tests.
     """
 
-    def test_one(self):
-        """"""
-        pass
+    def test_retry(self):
+        """Retry policy test"""
+        policy = RetryPolicy([
+            {"attempts": 1, "wait": "PT10S"},
+            {"attempts": 4, "wait": "PT1M"},
+            {"attempts": 5, "wait": "PT1H"}
+        ])
+
+        tens = datetime.timedelta(seconds=10)
+        onem = datetime.timedelta(minutes=1)
+        oneh = datetime.timedelta(hours=1)
+
+        # sequence of deltas to be returned
+        series = [tens, onem, onem, onem, onem, oneh, oneh, oneh, oneh, oneh, None, None]
+
+        for attempt in range(0, 12):
+            retry = policy.retry(attempt)
+            self.assertEqual(retry, series[attempt])
 
 
 if __name__ == '__main__':
