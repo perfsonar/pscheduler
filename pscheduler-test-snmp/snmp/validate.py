555325d804e8afe0775694ac59e9b69d3679463f
Adilah Ku <Adilah Ku>
2017-05-19 16:31:54 -0400

    added custom specs for diff versions of snmp

--- pscheduler-test-snmp/snmp/validate.py
+++ pscheduler-test-snmp/snmp/validate.py
@@ -5,36 +5,76 @@
 from pscheduler import json_validate
 
 def spec_is_valid(json):
-# QUESTION: Separate schema for each version
-    if json['version'] == '2c':
-        schema = {
-            "local": {
-                "VersionNumber": {
-                    "type": "string",
-                    "enum": [ "1", "2c", "3"]
-	            },
-            "type": "object",
-            "properties": {
-                "schema":       { "$ref": "#/pScheduler/Cardinal" },
-                "host":         { "$ref": "#/pScheduler/Host" },
-                "host-node":    { "$ref": "#/pScheduler/Host" },
-                "dest":         { "$ref": "#/pScheduler/Host" },
-                "version":      { "$ref": "#/local/VersionNumber"},
-                "community":    { "$ref": "#/pScheduler/String"},
-                "oid":          { "$ref": "#/pScheduler/String"},
-                "op":           { "$ref": "#/local/OptionType"},
-                "timeout":      { "$ref": "#/pScheduler/Duration" },
+
+    schema = {
+        "local": {
+            "VersionNumber": {
+                "type": "string",
+                "enum": [ "1", "2c", "3"]
+            },
+            "AuthProtocol": {
+                "type": "string",
+                "enum": [ "md5", "sha"]
+            },
+            "PrivProtocol": {
+                "type": "string",
+                "enum": [ "aes", "des"]
+            },
+            "SecurityLevel": {
+                "type": "string",
+                "enum": [ "noauthnopriv", "authnopriv", "authpriv"]
+            },
+            "SNMPv1Spec": {
+                "type": "object",
+                "properties": {
+                    "schema":       { "$ref": "#/pScheduler/Cardinal" },
+                    "host":         { "$ref": "#/pScheduler/Host" },
+                    "host-node":    { "$ref": "#/pScheduler/Host" },
+                    "dest":         { "$ref": "#/pScheduler/Host" },
+                    "version":      { "$ref": "#/local/VersionNumber"},
+                    "community":    { "$ref": "#/pScheduler/String"},
+                    "oid":          { "$ref": "#/pScheduler/String"},
+                    "timeout":      { "$ref": "#/pScheduler/Duration" },
                 },
-            "required": [
-                "version",
-                "community",
-                "dest",
-                "op",
-                ]
+                "required": [
+                    "version",
+                    "community",
+                    "dest",
+                    "oid"
+                    ]
+            },
+            "SNMPv3Spec": {
+                "type": "object",
+                "properties": {
+                    "schema":       { "$ref": "#/pScheduler/Cardinal" },
+                    "host":         { "$ref": "#/pScheduler/Host" },
+                    "host-node":    { "$ref": "#/pScheduler/Host" },
+                    "dest":         { "$ref": "#/pScheduler/Host" },
+                    "version":      { "$ref": "#/local/VersionNumber"},
+                    "oid":          { "$ref": "#/pScheduler/String"},
+                    "sn":           { "$ref": "#/pScheduler/String" },
+                    "ap":           { "$ref": "#/local/AuthProtocol" },
+                    "pp":           { "$ref": "#/local/PrivProtocol" },
+                    "ak":           { "$ref": "#/pScheduler/String" },
+                    "pk":           { "$ref": "#/pScheduler/String" },
+                    "sl":           { "$ref": "#/local/SecurityLevel" },
+                    "context":      { "$ref": "#/pScheduler/String" },
+                    "timeout":      { "$ref": "#/pScheduler/Duration" },
+                },
+                "required": [
+                    "version",
+                    "dest",
+                    "oid"
+                    ]
             }
-            
-    if json['version'] == '3':
-        pass
+        
+        },
+        "oneOf": [
+            { "$ref": "#/local/SNMPv1Spec" },
+            { "$ref": "#/local/SNMPv3Spec" }
+        ],
+        "additionalProperties": True
+    }
 
     return json_validate(json, schema)
 
