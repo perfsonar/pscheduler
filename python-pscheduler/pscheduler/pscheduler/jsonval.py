277286a07b461d4a38a58217c3e9a35e7e72f674
rygon <rygon@umich.edu>
2017-06-14 12:42:39 -0400

    Added args to the JQTransformSpecification as an AnyJSON

--- python-pscheduler/pscheduler/pscheduler/jsonval.py
+++ python-pscheduler/pscheduler/pscheduler/jsonval.py
@@ -256,7 +256,8 @@ __dictionary__ = {
         "type": "object",
         "properties": {
             "script":    { "$ref": "#/pScheduler/String" },
-            "output-raw": { "$ref": "#/pScheduler/Boolean" }
+            "output-raw": { "$ref": "#/pScheduler/Boolean" },
+			"args": { "$ref": "#/pScheduler/AnyJSON" }
         },
         "additionalProperties": False,
         "required": [ "script" ]
