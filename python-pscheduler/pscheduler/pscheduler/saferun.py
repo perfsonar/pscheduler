1091975adf4754eb982e8e8ace51a5c056e7645f
Mark Feit <mfeit@internet2.edu>
2017-09-11 12:34:35 -0400

    Re-exec the current program instead of re-calling the main function.  #548

--- python-pscheduler/pscheduler/pscheduler/saferun.py
+++ python-pscheduler/pscheduler/pscheduler/saferun.py
@@ -2,19 +2,25 @@
 # Safe Lambda Runner
 #
 
+import os
+import pickle
 import pscheduler
+import sys
 import time
 
+# Environment variable used to convey state between runs
+STATE_VARIABLE = "PSCHEDULER_SAFERUN_STATE"
 
 def safe_run(function,
              name=None,
              backoff=0.25,     # Backoff time increment
-             backoff_max=60,   # Longest allowable backoff
+             backoff_max=60.0, # Longest allowable backoff
              restart=True      # Call again if the function returns
              ):
     """
     Safely call a long-running lambda (usually a main program),
-    catching and logging exceptions.  The lambda will be re-called
+    catching and logging exceptions.  If an exception is thrown, the
+    calling program will be re-exec'd using the same arguments
     immediately if it simply returns or after a linearly-increasing
     backoff if it raises an exception.  The backoff always applies if
     the function has not yet run successfully and time will reset once
@@ -30,41 +36,85 @@ def safe_run(function,
                          quiet=True
                          )
 
-    initial_backoff = backoff
-    current_backoff = backoff
-    runs = 0
+    # Inherit state from the environment
 
-    while True:
+    if STATE_VARIABLE in os.environ:
 
         try:
-            started = pscheduler.time_now()
-            function()
-            runs += 1
-            if not restart:
-                break
+            depickled = pickle.loads(os.environ[STATE_VARIABLE])
 
-        except KeyboardInterrupt:
-            break
+            initial_backoff = depickled['initial_backoff']
+            assert type(initial_backoff) in [int, float]
+
+            current_backoff = depickled['current_backoff']
+            assert type(current_backoff) in [int, float]
+
+            runs = depickled['runs']
+            assert type(runs) == int
 
         except Exception as ex:
-            ran = pscheduler.time_now() - started
-            ran_seconds = pscheduler.timedelta_as_seconds(ran)
+            log.error("Failed to decode %s '%s': %s"
+                           % (STATE_VARIABLE, os.environ[STATE_VARIABLE], ex))
+            exit(1)
+    else:
 
-            log.error("Program threw an exception after %s", ran)
-            log.exception()
+        initial_backoff = backoff
+        current_backoff = backoff
+        runs = 0
 
-            # Running longer than the backoff is a good excuse to try
-            # starting over.
-            if ran_seconds > current_backoff and runs != 0:
-                currrent_backoff = initial_backoff
-                log.error("Restarting immediately.")
-            else:
-                log.error("Waiting %s seconds before restarting",
-                          current_backoff)
-                time.sleep(current_backoff)
-                if current_backoff < backoff_max:
-                    current_backoff += initial_backoff
-                log.error("Restarting")
+
+    # Run the function
+
+    do_restart = False
+
+    try:
+        started = pscheduler.time_now()
+        function()
+        runs += 1
+        do_restart = restart
+
+    except KeyboardInterrupt:
+        pass
+
+    except Exception as ex:
+        ran = pscheduler.time_now() - started
+        ran_seconds = pscheduler.timedelta_as_seconds(ran)
+
+        log.error("Program threw an exception after %s", ran)
+        log.exception()
+
+        # Running longer than the backoff is a good excuse to try
+        # starting over.
+        if ran_seconds > current_backoff and runs != 0:
+            currrent_backoff = initial_backoff
+        else:
+            log.error("Waiting %s seconds before restarting",
+                      current_backoff)
+            time.sleep(current_backoff)
+            if current_backoff < backoff_max:
+                current_backoff += initial_backoff
+
+        do_restart = True
+
+    if not do_restart:
+        log.error("Exiting")
+        exit(0)
+
+    log.error("Restarting: %s", sys.argv)
+
+    #
+    # Pickle the current state to pass along
+    #
+
+    to_pickle = {
+        'initial_backoff': initial_backoff,
+        'current_backoff': current_backoff,
+        'runs': runs
+    }
+    os.environ[STATE_VARIABLE] = pickle.dumps(to_pickle)
+
+    os.execvp(sys.argv[0], sys.argv)
+
 
 
 # Test program
@@ -73,7 +123,7 @@ if __name__ == "__main__":
 
     def foo():
         print "FOO"
-        time.sleep(3)
+        time.sleep(2)
         raise Exception("Yuck!")
 
-    safe_run(lambda: foo(), restart=False)
+    safe_run(foo, restart=False)
