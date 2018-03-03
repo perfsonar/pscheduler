aaa87977bb6d87d829ba7833a53cdabaac2360fb
Andy Lake <andy@es.net>
2018-03-03 09:55:20 -0500

    Fixing missing lines from latest merge

--- python-pscheduler/pscheduler/pscheduler/psjson.py
+++ python-pscheduler/pscheduler/pscheduler/psjson.py
@@ -170,12 +170,13 @@ def json_dump(obj, dest=None, pretty=False):
 class RFC7464Emitter(object):
     """Emit JSON documents to a file handle in RFC 7464 format"""
 
-    def __init__(self, handle):
+    def __init__(self, handle, timeout=None):
 
         if type(handle) != file:
             raise TypeError("Handle must be a file.")
 
         self.handle = handle
+        self.timeout = timeout
 
 
     def emit_text(self, text):
@@ -201,10 +202,11 @@ class RFC7464Emitter(object):
 class RFC7464Parser(object):
     """Iterable parser for reading streaming JSON from a file handle"""
 
-    def __init__(self, handle):
+    def __init__(self, handle, timeout=None):
         if type(handle) != file:
             raise TypeError("Handle must be a file.")
         self.handle = handle
+        self.timeout = timeout
 
     # PYTHON3: def __next__(self)
     def next(self):
