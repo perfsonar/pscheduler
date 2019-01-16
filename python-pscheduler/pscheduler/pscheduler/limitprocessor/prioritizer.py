d2fc9496674cbc19ddc0f9b0aa0306ca1add210d
Mark Feit <mfeit@internet2.edu>
2019-01-16 15:56:55 -0500

    More development, probably close to done.  #479

--- python-pscheduler/pscheduler/pscheduler/limitprocessor/prioritizer.py
+++ python-pscheduler/pscheduler/pscheduler/limitprocessor/prioritizer.py
@@ -49,31 +49,33 @@ class Prioritizer():
             "  .priority // default",
             ";",
 
-            # This will be embedded in the task.
-            "def priority:",
-            "  ." + self.PRIVATE_KEY + ".priority",
-            ";",
-
-            "def set($value):",
-            "  # TODO: Must be an integer",
-            "  ." + self.PRIVATE_KEY + ".priority = $value",
-            ";",
-
-            "def adjust($value):",
-            "  # TODO: Must be an integer",
-            "  ." + self.PRIVATE_KEY + ".priority += $value",
-            ";",
-
-            "def change($message):",
+            "def __change($message):",
             "  if $message != null",
             "  then",
             "    ." + self.PRIVATE_KEY + ".diags += "
-            "       [ \"\\($message | tostring) (\\(."
-                    + self.PRIVATE_KEY + ".priority))\" ]"
+            "       [ \"\\($message | tostring)\" ]"
             "  else",
             "    .",
             "  end",
             ";",
+
+            "def priority:",
+            "  ." + self.PRIVATE_KEY + ".priority",
+            ";",
+
+            "def set($value; $message):",
+            "  # TODO: Must be an integer",
+            "  ." + self.PRIVATE_KEY + ".priority = $value",
+            "  | __change(\"\\($message)  (Set to \\($value))\")",
+            ";",
+
+            "def adjust($value; $message):",
+            "  # TODO: Must be an integer",
+            "  ." + self.PRIVATE_KEY + ".priority += $value",
+            "  | __change(\"\\($message)  (\\(if $value > 0 then \"Up\" else \"Down\" end) \\($value | length) to \\(."
+            + self.PRIVATE_KEY + ".priority))\")",
+            ";",
+
 
             script
             ]
@@ -128,19 +130,21 @@ if __name__ == "__main__":
             ".",
 
             "# Start with the lower of the requested and default priorities",
-            "| set([default, requested] | min)",
-            "| change(\"Initial priority\")",
+            "| set([default, requested] | min; \"Initial priority\")",
 
 	    "# Friendly requesters get a small bump in priority.",
 	    "| if classifiers_has(\"friendlies\")",
-            "  then adjust(5) | change(\"Friendly requester\") else . end",
+            "  then adjust(5; \"Friendly requester\") else . end",
+
+	    "# Knock everyone off their high horses just because.",
+	    "| adjust(-1; \"We don't like you.\")",
 
 	    "# Allow at least the requested priority for those who are",
 	    "# allowed to do so.  Do this last in case things done",
 	    "# above push the priority higher than was requested",
 	    "| if classifiers_has(\"priority-positive\")",
 	    "  and requested > priority"
-	    "  then set(requested) | change(\"High requested priority\")",
+	    "  then set(requested; \"High requested priority\")",
             "  else . end",
 
 	    "# The end.  (This takes care of the no-comma-at-end problem)"
