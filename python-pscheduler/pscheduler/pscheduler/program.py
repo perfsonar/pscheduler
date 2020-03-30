3ddc164e06e228fe31c3936b132eae4c32ebcdbf
Mark Feit <mfeit@internet2.edu>
2020-03-30 21:10:33 +0000

    Invoke Python with /usr/bin/env #483 #974

--- python-pscheduler/pscheduler/pscheduler/program.py
+++ python-pscheduler/pscheduler/pscheduler/program.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """
 Functions for running external programs neatly
 """
--- python-pscheduler/pscheduler/tests/clockstate_test.py
+++ python-pscheduler/pscheduler/tests/clockstate_test.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """
 test for the Clockstate module.
 """
