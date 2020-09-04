776e5ade1353ad436e5bea386e521fedfd09ed18
Mark Feit <mfeit@internet2.edu>
2020-09-04 14:12:27 +0000

    Don't do time constraints on non-run tables; cleaner implementation.  #1047

--- pscheduler-server/pscheduler-server/api-server/pschedulerapiserver/stat.py
+++ pscheduler-server/pscheduler-server/api-server/pschedulerapiserver/stat.py
@@ -40,6 +40,22 @@ def stat_control_pause():
 
 
 
+def single_numeric_query(query, query_args = []):
+    """
+    Run a query that produces a single numeric result and return it.
+    """
+
+    cursor = dbcursor_query(query, query_args)
+
+    if cursor.rowcount == 0:
+        return not_found()
+
+    row = cursor.fetchone()
+    cursor.close()
+
+    return ok(str(row[0]))
+
+
 #
 # Archiving
 #
@@ -70,13 +86,15 @@ def stat_http_queue_length():
 
 
 
+
 #
 # Runs
 #
 
-
-def time_range_condition():
-    """Generate ANDs for a WHERE clause limiting start and end run times"""
+def single_run_query(query):
+    """
+    Do a query against the run table with time constraints.
+    """
 
     def absrel_arg(arg):
         value = arg_string(arg)
@@ -84,90 +102,70 @@ def time_range_condition():
             value = pscheduler.iso8601_absrel(value)
         return value
 
-
-    result = ""
-
     start = absrel_arg("start")
     end = absrel_arg("end")
 
     # Autocorrect upside down ranges.
-    if start > end:
+    if (start is not None) and (end is not None) and (start > end):
         (start, end) = (end, start)
 
     if start is not None:
-        result += " AND lower(times) >= '%s'" % (str(start))
+        query += " AND lower(times) >= '%s'" % (str(start))
 
     if end is not None:
-        result += " AND lower(times) <= '%s'" % (str(end))
+        query += " AND lower(times) <= '%s'" % (str(end))
 
-    return result
-
-
-def single_numeric_query(query, query_args = []):
-
-    try:
-        query += time_range_condition()
-    except ValueError as ex:
-        return bad_request(str(ex))
-
-    cursor = dbcursor_query(query, query_args)
-
-    if cursor.rowcount == 0:
-        return not_found()
-
-    row = cursor.fetchone()
-    cursor.close()
-
-    return ok(str(row[0]))
+    return single_numeric_query(query)
 
 
 
 @application.route("/stat/runs/pending", methods=['GET'])
 def stat_runs_pending():
-    return single_numeric_query("""SELECT COUNT(*) FROM run 
-                                   WHERE STATE = run_state_pending()""")
+    return single_run_query("""SELECT COUNT(*) FROM run 
+                               WHERE STATE = run_state_pending()""")
+
 
 @application.route("/stat/runs/on-deck", methods=['GET'])
 def stat_runs_on_deck():
-    return single_numeric_query("""SELECT COUNT(*) FROM run 
-                                   WHERE STATE = run_state_on_deck()""")
+    return single_run_query("""SELECT COUNT(*) FROM run 
+                               WHERE STATE = run_state_on_deck()""")
 
 @application.route("/stat/runs/running", methods=['GET'])
 def stat_runs_running():
-    return single_numeric_query("""SELECT COUNT(*) FROM run 
-                                   WHERE STATE = run_state_running()""")
+    return single_run_query("""SELECT COUNT(*) FROM run 
+                               WHERE STATE = run_state_running()""")
 
 @application.route("/stat/runs/cleanup", methods=['GET'])
 def stat_runs_cleanup():
-    return single_numeric_query("""SELECT COUNT(*) FROM run 
-                                   WHERE STATE = run_state_cleanup()""")
+    return single_run_query("""SELECT COUNT(*) FROM run 
+                               WHERE STATE = run_state_cleanup()""")
 
 @application.route("/stat/runs/finished", methods=['GET'])
 def stat_runs_finished():
-    return single_numeric_query("""SELECT COUNT(*) FROM run 
-                                   WHERE STATE = run_state_finished()""")
+    return single_run_query("""SELECT COUNT(*) FROM run 
+                               WHERE STATE = run_state_finished()""")
 
 @application.route("/stat/runs/overdue", methods=['GET'])
 def stat_runs_overdue():
-    return single_numeric_query("""SELECT COUNT(*) FROM run 
-                                   WHERE STATE = run_state_overdue()""")
+    return single_run_query("""SELECT COUNT(*) FROM run 
+                               WHERE STATE = run_state_overdue()""")
 
 @application.route("/stat/runs/missed", methods=['GET'])
 def stat_runs_missed():
-    return single_numeric_query("""SELECT COUNT(*) FROM run 
+    return single_run_query("""SELECT COUNT(*) FROM run 
                                    WHERE STATE = run_state_missed()""")
 
 @application.route("/stat/runs/failed", methods=['GET'])
 def stat_runs_failed():
-    return single_numeric_query("""SELECT COUNT(*) FROM run 
+    return single_run_query("""SELECT COUNT(*) FROM run 
                                    WHERE STATE = run_state_failed()""")
 
 @application.route("/stat/runs/preempted", methods=['GET'])
 def stat_runs_preempted():
-    return single_numeric_query("""SELECT COUNT(*) FROM run 
+    return single_run_query("""SELECT COUNT(*) FROM run 
                                    WHERE STATE = run_state_preempted()""")
 
 @application.route("/stat/runs/nonstart", methods=['GET'])
 def stat_runs_nonstart():
-    return single_numeric_query("""SELECT COUNT(*) FROM run 
+    return single_run_query("""SELECT COUNT(*) FROM run 
                                    WHERE STATE = run_state_nonstart()""")
