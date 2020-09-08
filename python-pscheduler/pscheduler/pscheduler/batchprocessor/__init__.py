ab90b692d0f6994ebba1d96d749c3ccf369ac95b
Mark Feit <mfeit@internet2.edu>
2020-09-08 15:24:32 +0000

    Added batch processor.  #1066

---
+++ python-pscheduler/pscheduler/pscheduler/batchprocessor/__init__.py
@@ -0,0 +1,5 @@
+#
+# Initialization for pScheduler Batch Processor Package
+#
+
+from .batchprocessor import *
--- python-pscheduler/pscheduler/pscheduler/exitstatus.py
+++ python-pscheduler/pscheduler/pscheduler/exitstatus.py
@@ -3,6 +3,7 @@ Functions for exiting scripts in a standard way.
 """
 
 import json
+import os
 import sys
 
 
@@ -47,3 +48,26 @@ def succeed_json(result=None):
     json.dump(result, sys.stdout)
     print()
     exit(0)
+
+
+
+def beta_feature(example=None):
+    """
+    Require that the environment be set for a beta feature and fail if
+    it isn't.
+    """
+
+    if example is None:
+        example = "pscheduler %s ..." % (os.path.basename(sys.argv[0]))
+
+    if "BETA" not in os.environ:
+        fail(
+            "This program is a beta feature and must be run with the\n"
+            "BETA environment variable set.\n"
+            "\n"
+            "For example:\n"
+            "$ BETA=1 %s\n"
+            "\n"
+            "This requirement will be removed in a future release."
+            % (example)
+        )
