9cfb0cf7250648e3e83882c15123fe681c0cfade
Kristofer Hallin <kristofer@sunet.se>
2020-12-04 20:07:59 +0100

    Make sure we have a supported EAP type.

--- pscheduler-test-802.1x/802.1x/validate.py
+++ pscheduler-test-802.1x/802.1x/validate.py
@@ -7,12 +7,18 @@ from pscheduler import json_validate
 
 def spec_is_valid(json):
     schema = {
+        "local": {
+            "eap_type": {
+                "type": "string",
+                "enum": ["PEAP", "MD5"]
+            }
+        },
         "type": "object",
         "properties": {
             "schema":    {"$ref": "#/pScheduler/Cardinal"},
             "timeout":   {"$ref": "#/pScheduler/Duration"},
             "interface": {"$ref": "#/pScheduler/String"},
-            "eap_type":  {"$ref": "#/pScheduler/String"},
+            "eap_type":  {"$ref": "#/local/eap_type"},
             "_username":  {"$ref": "#/pScheduler/String"},
             "_password":  {"$ref": "#/pScheduler/String"},
             "auth_inner":  {"$ref": "#/pScheduler/String"},
@@ -35,8 +41,6 @@ def result_is_valid(json):
         "properties": {
             "schema":     {"$ref": "#/pScheduler/Cardinal"},
             "succeeded":  {"$ref": "#/pScheduler/Boolean"},
-            "supplicant": {"$ref": "#/pScheduler/String"},
-            "dhclient":   {"$ref": "#/pScheduler/String"},
             "bssid":      {"$ref": "#/pScheduler/String"},
             "mac_addr":   {"$ref": "#/pScheduler/String"},
             "ip_addr":    {"$ref": "#/pScheduler/String"},
