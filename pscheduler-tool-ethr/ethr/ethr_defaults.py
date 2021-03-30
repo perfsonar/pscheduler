b3e6a05648e98e8ccaf3182e3ec874ec92221e1c
Daniel Neto <daniel.jsilvan@gmail.com>
2021-03-30 17:12:56 -0300

    Issue 1118 (#1135)
    Implemented loopback mode for throughput tests and tools.

---
+++ pscheduler-tool-ethr/ethr/ethr_defaults.py
@@ -0,0 +1,9 @@
+##########################################################
+# Contains variables shared across ethr tool components
+##########################################################
+
+# Default test time (in seconds)
+DEFAULT_DURATION = 20
+
+# The default ethr port for TCP & UDP tests
+DEFAULT_SERVER_PORT = 9999
