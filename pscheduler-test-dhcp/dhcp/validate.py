db995ea32bc3ae171ac5d844a62cf62728734618
Alyssa Bigley <abigley@umich.edu>
2021-08-03 18:46:04 +0000

    fixed result formatting

--- pscheduler-test-dhcp/dhcp/validate.py
+++ pscheduler-test-dhcp/dhcp/validate.py
@@ -49,7 +49,7 @@ def result_is_valid(json):
             "schema":     { "$ref": "#/pScheduler/Cardinal" },
             "succeeded":  { "$ref": "#/pScheduler/Boolean" },
             "time":       { "$ref": "#/pScheduler/Duration" },
-	        "ip_address": { "$ref": "#/pScheduler/AnyJSON" },
+            "ip_address": { "$ref": "#/pScheduler/AnyJSON" },
             },
         "required": [
             "time",
