09971ae2cb16a7a54f2b194eae9dcd1ef39dcd62
Mark Feit <mfeit@internet2.edu>
2021-12-13 18:07:42 -0500

    Issue 1107 (#1227)
    * Issue 1107 (#1182)
    
    * Early development for MTU   #1107
    
    * Packaged python-scapy #1107
    
    * Packaged python-scapy #1107
    
    * Set up basic test
    
    * Use python socket instead of scapy
    
    Note that this only works on Linux
    
    * Finish writing fwmtu tool #1107
    
    * Remove Scapy (not needed anymore)
    
    * Update cli-to-spec with usage
    
    * Add run_input
    
    (in case we need future testing)
    
    * Final integration.
    
    Co-authored-by: Frank Wang <47217770+frnkwang@users.noreply.github.com>

---
+++ pscheduler-test-mtu/mtu/validate.py
@@ -0,0 +1,57 @@
+#
+# Validator for a pScheduler Test
+#
+
+#
+# Development Order #3:
+#
+# This file determines the required and optional data types which are 
+# allowed to be in the test spec, result, and limit. This is used
+# for validation of these structures.
+#
+# Several existing datatypes are available for use at:
+# pscheduler/python-pscheduler/pscheduler/pscheduler/jsonval.py
+# 
+
+from pscheduler import json_validate
+
+MAX_SCHEMA = 1
+
+def spec_is_valid(json):
+
+    schema = {
+        "type": "object",
+        # schema, host, host-node, and timeout are standard,
+        # and should be included
+        "properties": {
+            "schema":       { "$ref": "#/pScheduler/Cardinal" },
+            "source":       { "$ref": "#/pScheduler/Host" },
+            "source-node":  { "$ref": "#/pScheduler/Host" },
+            "dest":         { "$ref": "#/pScheduler/Host" },
+            "port":         { "$ref": "#/pScheduler/IPPort" },
+        },
+        # If listed here, data of this type MUST be in the test spec
+        "required": [
+            "dest",
+            ],
+        # Set to false if ONLY required options should be used
+        "additionalProperties": False
+    }
+
+    return json_validate(json, schema, max_schema=MAX_SCHEMA)
+
+def result_is_valid(json):
+    schema = {
+        "type": "object",
+        "properties": {
+            "schema":     { "$ref": "#/pScheduler/Cardinal" },
+            "succeeded":  { "$ref": "#/pScheduler/Boolean" },
+            "mtu":        { "$ref": "#/pScheduler/Cardinal" },
+            },
+        "required": [
+            "succeeded",
+            "mtu",
+            ]
+        }
+    return json_validate(json, schema)
+
