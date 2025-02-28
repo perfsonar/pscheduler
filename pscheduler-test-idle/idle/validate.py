d7b906453eae46611759c60595ee44122c5d7f19
Mark Feit <mfeit@internet2.edu>
2025-02-28 21:25:10 +0000

    Convert test-idle to modern validation.  #1526

--- pscheduler-test-idle/idle/validate.py
+++ pscheduler-test-idle/idle/validate.py
@@ -8,73 +8,84 @@
 # changes MUST be made in 'spec-format' and 'result-format' to make
 # them capable of formatting the new specifications and results.
 
-from pscheduler import json_validate
+from pscheduler import json_standard_template_max_schema
+from pscheduler import json_validate_from_standard_template
 
-MAX_SCHEMA = 2
 
+SPEC_SCHEMA = {
+    "local": { },
 
-def spec_is_valid(json):
-    schema = {
-        "local": {
-            "v1": {
-                "type": "object",
-                "properties": {
-                    "schema":           { "type": "integer", "enum": [ 1 ] },
-                    "duration":         { "$ref": "#/pScheduler/Duration" },
-                    "host":             { "$ref": "#/pScheduler/Host" },
-                    "host-node":        { "$ref": "#/pScheduler/URLHostPort" },
-                    "parting-comment":  { "$ref": "#/pScheduler/String" },
-                    "starting-comment": { "$ref": "#/pScheduler/String" },
-                },
-                "additionalProperties": False,
-                "required": [
-                    "duration",
-                ]
+    "versions": {
+
+        "1": {
+            "type": "object",
+            "properties": {
+                "schema":           { "type": "integer", "enum": [ 1 ] },
+                "duration":         { "$ref": "#/pScheduler/Duration" },
+                "host":             { "$ref": "#/pScheduler/Host" },
+                "host-node":        { "$ref": "#/pScheduler/URLHostPort" },
+                "parting-comment":  { "$ref": "#/pScheduler/String" },
+                "starting-comment": { "$ref": "#/pScheduler/String" },
             },
-            "v2": {
-                "type": "object",
-                "properties": {
-                    "schema":           { "type": "integer", "enum": [ 2 ] },
-                    "duration":         { "$ref": "#/pScheduler/Duration" },
-                    "host":             { "type": "array",
-                                          "items": { "$ref": "#/pScheduler/Host" },
-                                          "minItems" : 1 },
-                    "parting-comment":  { "$ref": "#/pScheduler/String" },
-                    "starting-comment": { "$ref": "#/pScheduler/String" },
-                },
-                "additionalProperties": False,
-                "required": [
-                    "duration",
-                ]
-            }
-        }
-    }
+            "additionalProperties": False,
+            "required": [
+                "duration",
+            ]
+        },
 
-    # Build a temporary structure with a reference that points
-    # directly at the validator for the specified version of the
-    # schema.  Using oneOf or anyOf results in error messages that are
-    # difficult to decipher.
-    temp_schema = {
-        "local": schema["local"],
-        "$ref":"#/local/v%s" % json.get("schema", 1)
-    }
-
-    return json_validate(json, temp_schema, max_schema=MAX_SCHEMA)
-
-def result_is_valid(json):
-    schema = {
-        "type": "object",
-        "properties": {
-            "schema":           { "$ref": "#/pScheduler/Cardinal" },
-            "duration":         { "$ref": "#/pScheduler/Duration" },
-            "succeeded":        { "$ref": "#/pScheduler/Boolean" },
-            "error":            { "$ref": "#/pScheduler/String" },
-            "diags":            { "$ref": "#/pScheduler/String" }
+        "2": {
+            "type": "object",
+            "properties": {
+            "schema":           { "type": "integer", "enum": [ 2 ] },
+                "duration":         { "$ref": "#/pScheduler/Duration" },
+                "host":             { "type": "array",
+                                      "items": { "$ref": "#/pScheduler/Host" },
+                                      "minItems" : 1 },
+                "parting-comment":  { "$ref": "#/pScheduler/String" },
+                "starting-comment": { "$ref": "#/pScheduler/String" },
             },
-        "additionalProperties": False,
-        "required": [
-            "duration",
-            "succeeded",
+            "additionalProperties": False,
+            "required": [
+                "duration",
             ]
         }
-    return json_validate(json, schema)
+    }
+}
+
+def spec_max_schema():
+    return json_standard_template_max_schema(SPEC_SCHEMA)
+
+def spec_is_valid(json):
+    return json_validate_from_standard_template(json, SPEC_SCHEMA)
+
+
+RESULT_SCHEMA = {
+
+    "local": {},
+
+    "versions": {
+
+        "1": {
+            "type": "object",
+            "properties": {
+                "schema":           { "$ref": "#/pScheduler/Cardinal" },
+                "duration":         { "$ref": "#/pScheduler/Duration" },
+                "succeeded":        { "$ref": "#/pScheduler/Boolean" },
+                "error":            { "$ref": "#/pScheduler/String" },
+                "diags":            { "$ref": "#/pScheduler/String" }
+            },
+            "additionalProperties": False,
+            "required": [
+                "duration",
+                "succeeded",
+            ]
+        }
+    }
+}
+
+
+def result_max_schema():
+    return json_standard_template_max_schema(RESULT_SCHEMA)
+
+def result_is_valid(json):
+    return json_validate_from_standard_template(json, RESULT_SCHEMA)
