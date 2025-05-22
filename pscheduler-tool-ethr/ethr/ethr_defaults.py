1d9854b9a90963f8fff73ccea9c26263f8e319ea
Mark Feit <mfeit@internet2.edu>
2025-05-22 16:25:47 +0000

    Upgrade Ethr to 1.0.0.  Still needs Debian-compatible patch.  #1538

--- pscheduler-tool-ethr/ethr/ethr_defaults.py
+++ pscheduler-tool-ethr/ethr/ethr_defaults.py
@@ -1,9 +1,26 @@
 ##########################################################
-# Contains variables shared across ethr tool components
+# Contains variables shared across Ethr tool components
 ##########################################################
 
-# Default test time (in seconds)
-DEFAULT_DURATION = 20
+# Default test time
+DEFAULT_DURATION = 10
 
-# The default ethr port for TCP & UDP tests
+# Default link RTT in seconds.  This is a rough approximation of RTT
+# for the longest possible terrestrial path.
+DEFAULT_LINK_RTT = "PT0.200S"
+
+
+# Default number of seconds before client will start to allow server time to boot
+DEFAULT_WAIT_SLEEP = 3
+
+# Default number of seconds to let the server spin down
+DEFAULT_SERVER_SHUTDOWN = 2
+
+# The default control port
 DEFAULT_SERVER_PORT = 5201
+
+# The default location of the config file
+CONFIG_FILE = '/etc/pscheduler/tool/ethr.conf'
+
+# Default install location of Ethr
+DEFAULT_ETHR_PATH = 'ethr'
