5e8b105e8c3770c4f598cd1b557ecb3623dd8ca0
Valters G <valtersg2@gmail.com>
2025-10-23 12:27:08 +0300

    Added throughput tool: qperf

---
+++ pscheduler-tool-qperf/qperf/qperf_defaults.py
@@ -0,0 +1,25 @@
+##########################################################
+# Contains variables shared across qperf tool components
+##########################################################
+
+# Default test time
+DEFAULT_DURATION = 'PT10S'
+
+# Default link RTT.
+DEFAULT_LINK_RTT = 'PT0.200S'
+
+# Default number of seconds before client will start to allow server time to boot
+DEFAULT_WAIT_SLEEP = 5
+
+# Default number of seconds to let the server spin down
+DEFAULT_SERVER_SHUTDOWN = 2
+
+# The default qperf control port
+DEFAULT_PORT = 20000
+DEFAULT_DATA_PORT = 20001
+
+# The default location of the config file
+CONFIG_FILE = '/etc/pscheduler/tool/qperf.conf'
+
+# Default install location of qperf
+DEFAULT_QPERF_PATH = '/usr/bin/qperf'
