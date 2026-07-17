087be75584c6dc7f49a88034bf663a1bd04f558d
Mark Feit <mfeit@internet2.edu>
2026-07-17 19:01:01 +0000

    Do extended JSON-invalid message formatting everywhere there's validation.

--- python-pscheduler/pscheduler/pscheduler/plugins.py
+++ python-pscheduler/pscheduler/pscheduler/plugins.py
@@ -97,13 +97,18 @@ def enumerated_spec_is_valid(proposed, plugin_type, enumerate_part, plugin=None,
                             format_checker=jsonschema.draft7_format_checker
         )
     except jsonschema.exceptions.ValidationError as ex:
-        try:
-            message = ex.schema["x-invalid-message"].replace("%s", ex.instance)
-        except (KeyError, TypeError):
-            message = ex.message
 
+        message = ex.schema.get('x-invalid-message', ex.message).replace("%s", str(ex.instance))
+        examples = [ str(item) for item in ex.schema.get('examples',[]) ]
+        if examples:
+            message += '  Try something like '
+            if len(examples) > 1:
+                message += f'''{', '.join(examples[:-1])} or {examples[-1]}.'''
+            else:
+                message += f'''{examples[0]}.'''
         path = "/".join([str(x) for x in ex.absolute_path])
-        return (False, "At /%s: %s" % (path, message))
+
+        return (False, f'At /{path}: {message}')
 
     # If the caller gave us a final hook to validate semantics rather
     # than structure, call that.  Otherwise, all is good.
