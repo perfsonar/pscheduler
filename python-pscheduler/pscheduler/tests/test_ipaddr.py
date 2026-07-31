c04bdf6bc51f3b51df4f96dd68c922a50ac97ed6
Mark Feit <mfeit@internet2.edu>
2026-07-31 16:40:30 +0000

    Don't use DNS while testing ip_addr_version.  #1641

--- python-pscheduler/pscheduler/tests/test_ipaddr.py
+++ python-pscheduler/pscheduler/tests/test_ipaddr.py
@@ -74,25 +74,15 @@ class TestIpaddr(PschedTestBase):
 
         self.assertEqual(ip_addr_version('127.0.0.1', ip_version=4), (4, '127.0.0.1'))
         self.assertEqual(ip_addr_version('127.0.0.1', ip_version=6), (None, None))
-        self.assertEqual(ip_addr_version('a1.nv.perfsonar.net', ip_version=4), (4, '127.0.0.1'))
-        self.assertEqual(ip_addr_version('a1.nv.perfsonar.net', ip_version=6), (None, None))
 
         self.assertEqual(ip_addr_version('::1', ip_version=6), (6, '::1'))
         self.assertEqual(ip_addr_version('::1', ip_version=4), (None, None))
-        self.assertEqual(ip_addr_version('aaaa1.nv.perfsonar.net', ip_version=6), (6, 'fc00::1'))
-        self.assertEqual(ip_addr_version('aaaa1.nv.perfsonar.net', ip_version=4), (None, None))
 
         # By DNS name.  These are guaranteed to be stable as long as
         # nobody breaks the perfsonar.net zone.
 
-        self.assertEqual(ip_addr_version('a1.nv.perfsonar.net'), (4, '127.0.0.1'))
         self.assertEqual(ip_addr_version('a1.nv.perfsonar.net', resolve=False), (None, None))
-
-        self.assertEqual(ip_addr_version('aaaa1.nv.perfsonar.net'), (6, 'fc00::1'))
         self.assertEqual(ip_addr_version('aaaa1.nv.perfsonar.net', resolve=False), (None, None))
-
-        # Note that we can't test a-aaaa1.nv because we don't know
-        # what the local system will prefer.
 
 
 if __name__ == '__main__':
